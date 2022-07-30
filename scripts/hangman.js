b0529fe6dd04b522c70c0fcd03e9a4f9caaa8c52
bayoticstudios <abdullahishobaloju@gmail.com>
2022-07-31 00:10:41 +0100

    first commit

---
+++ scripts/hangman.js
@@ -0,0 +1,110 @@
+
+
+class Hangman {
+    constructor(word, remainingGuesses) {
+        this.word = word.toLowerCase().split('');
+        this.remainingGuesses = remainingGuesses;
+        this.guessedLetters = [];
+        this.status = 'playing';
+    }
+
+    get puzzle() {
+        let puzzle = '';
+        this.word.forEach((letter) => {
+            if (this.guessedLetters.includes(letter) || letter === ' ') {
+                puzzle += letter;
+            } else {
+                puzzle += '-'
+            }
+        })
+        return puzzle;
+    }
+
+    makeGuess(guess) {
+        guess = guess.toLowerCase();
+        const isUnique = !this.guessedLetters.includes(guess);
+        const isBadGuess = !this.word.includes(guess);
+
+        if (this.status !== 'playing') {
+            return
+        }
+
+        if (isUnique) {
+            this.guessedLetters.push(guess)
+        }
+
+        if (isUnique && isBadGuess) {
+            this.remainingGuesses--
+        }
+        this.calculateStatus();
+    }
+
+    get statusMessage() {
+        if (this.status === 'playing') {
+            return `Remaining Guesses: ${this.remainingGuesses}`
+        } else if (this.status === 'failed') {
+            return `Nice try! the words were "${this.word.join('')}" `
+        } else {
+            return 'You Passed The puzzle!'
+        }
+    }
+
+    calculateStatus() {
+        const finished = this.word.every((letter) => this.guessedLetters.includes(letter) || letter === ' ')
+
+        if (this.remainingGuesses === 0) {
+            this.status = 'failed'
+        } else if (finished) {
+            this.status = 'finished'
+        } else {
+            this.status = 'playing'
+        }
+    }
+
+}
+const getPuzzle = async (wordCount) => {
+    const response = await fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    if (response.status === 200) {
+        const data = await response.json()
+        return data.puzzle
+    } else {
+        throw new Error('Unable to fetch puzzle')
+    }
+}
+
+
+let game1
+const puzzleDIV = document.querySelector('#puzzle');
+const remainingDIV = document.querySelector('#guesses');
+
+window.addEventListener('keypress', (e) => {
+
+    const guess = String.fromCharCode(e.charCode);
+    game1.makeGuess(guess);
+    render()
+})
+
+const render = () => {
+    puzzleDIV.innerHTML = ''
+    remainingDIV.textContent = game1.statusMessage;
+
+    game1.puzzle.split('').forEach((letter) => {
+        const letterEl = document.createElement("span");
+        letterEl.textContent = letter
+        puzzleDIV.appendChild(letterEl)
+    })
+}
+
+const startGame = async () => {
+    const puzzle = await getPuzzle('3')
+    game1 = new Hangman(puzzle, 5)
+    render()
+}
+
+document.querySelector('#reset').addEventListener('click', startGame)
+startGame()
+
+
+
+
+
